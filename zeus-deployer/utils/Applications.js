057899f4abc793b3436e2ab8fb93a37104b31f89
ThuF <jordan.p.pavlov@gmail.com>
2018-05-28 18:56:43 +0000

    Adds application persistence

--- zeus-deployer/utils/Applications.js
+++ zeus-deployer/utils/Applications.js
@@ -1,16 +1,29 @@
+var dao = require('zeus-applications/data/dao/Applications');
 var Deployments = require('zeus-deployer/utils/Deployments');
 var Services = require('zeus-deployer/utils/Services');
 var Credentials = require('zeus-deployer/utils/Credentials');
+var ApplicationContainers = require('zeus-deployer/utils/application/Containers');
+var ApplicationServices = require('zeus-deployer/utils/application/Services');
 
-exports.create = function(templateId, clusterId, applicationName) {
+exports.create = function(templateId, clusterId, name) {
 	var credentials = Credentials.getCredentials(clusterId);
 
-	var deployment = Deployments.create(credentials.server, credentials.token, credentials.namespace, templateId, applicationName);
-	var service = Services.create(credentials.server, credentials.token, credentials.namespace, templateId, applicationName);
+	var deployment = Deployments.create(credentials.server, credentials.token, credentials.namespace, templateId, name);
+	var services = Services.create(credentials.server, credentials.token, credentials.namespace, templateId, name);
+
+	var applicationId = dao.create({
+		'Template': templateId,
+		'Cluster': clusterId,
+		'Name': name
+	});
+
+
+	ApplicationContainers.create(applicationId, deployment);
+	ApplicationServices.create(applicationId, services);
 
 	return {
 		'deployment': deployment,
-		'service': service
+		'services': services
 	};
 };
 
