6de6772c43c513f51ee41ded16ac3cd4b37c0eb9
ThuF <jordan.p.pavlov@gmail.com>
2018-05-28 13:11:52 +0000

    Adds "Deployer" util

--- zeus-deployer/api/Deployments.js
+++ zeus-deployer/api/Deployments.js
@@ -1,25 +1,11 @@
 var rs = require('http/v3/rs');
-var Pods = require('kubernetes/api/v1/Pods');
 
-var DeploymentsDao = require('zeus-deployer/data/dao/Deployments');
+var Deployer = require('zeus-deployer/utils/Deployer');
 
 rs.service()
 	.resource('')
 		.get(function(ctx, request, response) {
-//			var server = 'https://192.168.99.100:8443';
-//			var token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJ6ZXVzIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6InpldXMtdG9rZW4tYno4N3giLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiemV1cyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjVlNWFmZDEyLTYyNGUtMTFlOC1hOGM5LTA4MDAyNzVlNmQyNyIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDp6ZXVzOnpldXMifQ.XcMNfil2s_uWRThwSEOcv8TUuJbphupVn79r-7VVeMzDJQWEvEQKvG65bs90uf01uwpVHplQdVVMcOEKQj-P3jNC5EjZmLkLtKIJeLu8T54cliPa_sfoYHasBlPEoxE0pT01nN6Jlo-rdDlO_AGbFlgd0RKJN0_nxsLA8Pn3YbNCN5yaPsGfjGpcFk8Req8tgLKzCwTwvrWNgGfnG_VJHk8LTuBDYQiaya9S3GDDMT1v-h5CAkib8k1l-vlRwJyXEJx-2weRCku9lReU4cJN19Lp_A6jDaT6KoPR6TSSn3aGa9EJtJrx8i_vs7ODf2HhbQVBBaz-xoOrnnR6omtDpw';
-//
-//			var podsApi = new Pods(server, token, 'zeus');
-//			var pods = JSON.stringify(podsApi.list())
-
-			var templateId = parseInt(request.getParameter('templateId'));
-			var containers = DeploymentsDao.getContainers(templateId);
-			var services = DeploymentsDao.getServices(templateId);
-			var variables = DeploymentsDao.getVariables(templateId);
-			response.println(JSON.stringify({
-				'containers': containers,
-				'services': services,
-				'variables': variables
-			}));
+			var deployment = Deployer.deploy(1);
+			response.println(JSON.stringify(deployment));
 		})
 .execute();
---
+++ zeus-deployer/utils/Deployer.js
@@ -0,0 +1,48 @@
+var Deployments = require('kubernetes/apis/apps/v1beta1/Deployments');
+
+exports.deploy = function(templateId) {
+	var server = 'https://192.168.99.101:8443';
+	var token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJ6ZXVzIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6InpldXMtdG9rZW4teHZjenYiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiemV1cyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjQ1ZWFjYWEwLTYyNzYtMTFlOC1iNzAwLTA4MDAyNzVmODhlMSIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDp6ZXVzOnpldXMifQ.ndlt6o8l5-DJc9-jNuY7D4wmJo4AAW2uaNt5eVgu0JMoEo8-4HO0rnDXsE02aw3ueyhLKG2rG-Tn5fLtqqx7Qx40w57mKxhliZLMoGj-0oECNwbN0YVaozs46NSzjBd4cIp1au77mjVjXHE8Sf3lJNVZ7je7Wlqdfd8f3k85lNmI8ON_5pTJX0Vu-G0Ql07qvBSFohn8GqBGNfVipokv9dSAZszofAeO4CwQ2UWAq_hlVoCuERU9oyA0FvZCnIxoe0O9z41X1M7MEViOSYWFFM7zmBdYRt1uCIsjxO0cguv4fgp6I3yAIk7SmYvaV0DHykHdvklmbbtwstcGpuyUag';
+	var deploymentsApi = new Deployments(server, token, 'zeus');
+
+	var deploymentEntity = {
+		'apiVersion': 'apps/v1beta1',
+		'kind': 'Deployment',
+		'metadata': {
+			'name': 'test',
+			'labels': {
+				'app': 'test'
+			}
+		},
+		'spec': {
+			'replicas': 1,
+			'selector': {
+				'matchLabels': {
+					'app': 'test'
+				}
+			},
+			'template': {
+				'metadata': {
+					'labels': {
+						'app': 'test'
+					}
+				},
+				'spec': {
+					'containers': [{
+						'name': 'dirigible',
+						'image': 'dirigiblelabs/dirigible-tomcat:latest',
+						'ports': [{
+							'containerPort': 8080
+						}]
+					}]
+				}
+			}
+		}
+	};
+
+	return deploymentsApi.create(deploymentEntity);
+};
+
+exports.undeploy = function(templateId) {
+	
+};
