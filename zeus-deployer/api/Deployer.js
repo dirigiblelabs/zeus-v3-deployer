583d2eb27071bbd63bcae6887c50d0eacf64cd34
ThuF <jordan.p.pavlov@gmail.com>
2018-08-03 09:43:32 +0000

    Adds Deployer delete function

--- zeus-deployer/api/Deployer.js
+++ zeus-deployer/api/Deployer.js
@@ -15,10 +15,10 @@ rs.service()
 			var application = request.getJSON();
             var credentials = Credentials.getCredentials(application.settings.clusterId);
 
-            result.deployment = create(credentials, Deployments, application.deployment);
-            result.statefulSet = create(credentials, StatefulSets, application.statefulSet);
-            result.service = create(credentials, Services, application.service);
-            result.ingress = create(credentials, Ingresses, application.ingress);
+            result.deployment = createResource(credentials, Deployments, application.deployment);
+            result.statefulSet = createResource(credentials, StatefulSets, application.statefulSet);
+            result.service = createResource(credentials, Services, application.service);
+            result.ingress = createResource(credentials, Ingresses, application.ingress);
 
             // TODO: Create Application Entry!
             // var name = application.settings.applicationName;
@@ -42,104 +42,36 @@ rs.service()
 
             response.println(JSON.stringify(result));
 		})
-	.resource('{id}')
+	.resource('')
 		.delete(function(ctx, request, response) {
-            // TODO: Delete Resources
+            var result = {};
+            var clusterId = request.getParameter('clusterId');
+            var deploymentName = request.getParameter('deploymentName');
+            var statefulSetName = request.getParameter('statefulSetName');
+            var serviceName = request.getParameter('serviceName');
+            var ingressName = request.getParameter('ingressName');
+            var credentials = Credentials.getCredentials(clusterId);
+
+            result.deployment = deleteResource(credentials, Deployments, deploymentName);
+            result.statefulSet = deleteResource(credentials, StatefulSets, statefulSetName);
+            result.service = deleteResource(credentials, Services, serviceName);
+            result.ingress = deleteResource(credentials, Ingresses, ingressName);
+
+            response.println(JSON.stringify(result));
 		})
 .execute();
 
 
-function create(credentials, api, entity) {
+function createResource(credentials, api, entity) {
     if (entity) {
         entity.namespace = credentials.namespace;
         var resource = api.build(entity);
         return api.create(credentials.server, credentials.token, credentials.namespace, resource);
     }
-    return null;
 }
-// var entity = {
-//     'deployment': {
-//         'name': 'demo',
-//         'namespace': 'zeus',
-//         'application': 'zeus',
-//         'replicas': 1,
-//         'containers': [{
-//             'name': 'dirigible',
-//             'image': 'dirigiblelabs/dirigible:latest',
-//             'port': 8080,
-//             'env': [{
-//                 'name': 'KEYCLOAK_CONFIDENTIAL_PORT',
-//                 'value': 443
-//             }, {
-//                 'name': 'KEYCLOAK_SSL_REQUIRED',
-//                 'value': 'none'
-//             }, {
-//                 'name': 'KEYCLOAK_CLIENT_ID',
-//                 'value': 'ide'
-//             }, {
-//                 'name': 'KEYCLOAK_REALM',
-//                 'value': 'applications'
-//             }, {
-//                 'name': 'KEYCLOAK_AUTH_SERVER_URL',
-//                 'value': 'https://auth.ingress.dev.promart.shoot.canary.k8s-hana.ondemand.com/auth'
-//             }]
-//         }]
-//     },
-//     'statefulSet': {
-//         'name': 'demo',
-//         'namespace': 'zeus',
-//         'application': 'demo',
-//         'replicas': 1,
-//         'storage': '1Gi',
-//         'serviceName': 'zeus-http',
-//         'containers': [{
-//             'name': 'dirigible',
-//             'image': 'dirigiblelabs/dirigible:latest',
-//             'port': 8080,
-//             'mountPath': '/usr/local/tomcat/dirigible',
-//             'env': [{
-//                 'name': 'KEYCLOAK_CONFIDENTIAL_PORT',
-//                 'value': 443
-//             }, {
-//                 'name': 'KEYCLOAK_SSL_REQUIRED',
-//                 'value': 'none'
-//             }, {
-//                 'name': 'KEYCLOAK_CLIENT_ID',
-//                 'value': 'ide'
-//             }, {
-//                 'name': 'KEYCLOAK_REALM',
-//                 'value': 'applications'
-//             }, {
-//                 'name': 'KEYCLOAK_AUTH_SERVER_URL',
-//                 'value': 'https://auth.ingress.dev.promart.shoot.canary.k8s-hana.ondemand.com/auth'
-//             }]
-//         }]
-//     },
-//     'service': {
-//         'name': 'demo-http',
-//         'namespace': 'zeus',
-//         'application': 'demo',
-//         'type': 'NodePort',
-//         'port': 8080
-//     },
-//     'ingress': {
-//         'name': 'demo',
-//         'namespace': 'zeus',
-//         'application': 'demo',
-//         'host': 'demo.ingress.dev.promart.shoot.canary.k8s-hana.ondemand.com',
-//         'serviceName': 'demo-http',
-//         'servicePort': 8080
-//     }
-// };
 
-// var deployment = Deployments.build(entity.deployment);
-// var statefulSet = StatefulSets.build(entity.statefulSet);
-// var service = Services.build(entity.service);
-// var ingress = Ingresses.build(entity.ingress);
-
-// response.println(JSON.stringify({
-//     'deployment': deployment,
-//     'statefulSet': statefulSet,
-//     'service': service,
-//     'ingress': ingress
-// }));
+function deleteResource(credentials, api, name) {
+    if (name) {
+        return api.delete(credentials.server, credentials.token, credentials.namespace, name);
+    }
+}
